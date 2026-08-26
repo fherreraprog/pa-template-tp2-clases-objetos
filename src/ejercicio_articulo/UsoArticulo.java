44106ce285b92aa2089d1eda02ee07a84d3f11c3
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2026-08-26 22:23:55 +0000

    fix: eliminar target

--- src/ejercicio_articulo/UsoArticulo.java
+++ src/ejercicio_articulo/UsoArticulo.java
@@ -2,6 +2,6 @@ package ejercicio_articulo;
 
 public class UsoArticulo {
     public static void main(String[] args) {
-        System.out.println("Probando la clase Articulo...");
+        System.out.println("Probando la clase Articulo..");
     }
 }
